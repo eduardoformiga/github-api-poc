509a9219fbb2b53ef76c10d427b2ce9efcad066a
eduardoformiga <eduardo.formiga@gmail.com>
2019-10-03 01:51:35 -0300

    chore: refactor github service with baseUrl

--- src/services/github.services.js
+++ src/services/github.services.js
@@ -1,13 +1,15 @@
 import axios from 'axios'
 
+const baseUrl = 'https://api.github.com'
+
 export const getReposByUser = async user => {
-  return await axios.get(`https://api.github.com/users/${user}/repos`)
+  return await axios.get(`${baseUrl}/users/${user}/repos`)
 }
 
 export const getStarredByUser = async user => {
-  return await axios.get(`https://api.github.com/users/${user}/starred`)
+  return await axios.get(`${baseUrl}/users/${user}/starred`)
 }
 
 export const getUserProfile = async user => {
-  return await axios.get(`https://api.github.com/users/${user}`)
+  return await axios.get(`${baseUrl}/users/${user}`)
 }
